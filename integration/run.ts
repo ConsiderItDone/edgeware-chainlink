dce6df36bbcb7f9c50b36c710be5453a94e6227a
Mikhail Reenko <reenko@yandex.ru>
2021-05-06 20:06:32 +0700

    Update integration script

--- integration/run.ts
+++ integration/run.ts
@@ -1,10 +1,37 @@
 import axios, {AxiosResponse} from 'axios';
 import to from 'await-to-js';
-
-const HOST = 'http://127.0.0.1:8080';
-const NODE = '0x0EC5dA4Da96C66c33D55E3bD056c2deA03929B64';
+const fs = require('fs');
 
 (async () : Promise<any> => {
+    const rawData = fs.readFileSync('./cypress.json');
+    const config = JSON.parse(rawData)?.env;
+
+    const HOST = process.env.HOST || config.HOST;
+    const CHAINLINK_HOST = process.env.CHAINLINK_URL || config.CHAINLINK_URL;
+
+    const login = await axios.post(`${CHAINLINK_HOST}/sessions`, {
+        email: process.env.CHAINLINK_EMAIL || config.CHAINLINK_EMAIL,
+        password: process.env.CHAINLINK_PASSWORD || config.CHAINLINK_PASSWORD,
+    }, {
+        headers:  {
+            "accept": "application/json",
+            "content-type": "application/json",
+        },
+        withCredentials: true,
+    });
+    const cookies = login.headers['set-cookie'];
+
+    const keys = await axios.get(`${CHAINLINK_HOST}/v2/keys/eth`, {
+        headers: {
+            cookie: cookies.join(';'),
+        }
+    })
+    const node = keys?.data?.data[0].id;
+    if (!node) {
+        throw new Error('Can not receive keys');
+    }
+    console.log('Node:', node);
+
     const [errToken, responseToken] = await to(axios.get(`${HOST}`));
     if (errToken) {
         throw new Error('edgeware-chainlink-contract not allowed');
@@ -13,31 +40,44 @@ const NODE = '0x0EC5dA4Da96C66c33D55E3bD056c2deA03929B64';
     const token = (responseToken as AxiosResponse).data;
     console.log('Token:', token);
 
-    const [errOracle, responseOracle] = await to(axios.get(`${HOST}/deploy/oracle?node=${NODE}`));
+    const [errOracle, responseOracle] = await to(axios.get(`${HOST}/deploy/oracle?node=${node}`));
     if (errOracle) {
-        throw new Error(errOracle.message);
+        throw new Error(errOracle?.message);
     }
 
     const oracle = (responseOracle as AxiosResponse).data;
     console.log('Oracle:', oracle);
 
-    // TODO: Create job with new oracle address
-    // docker exec -it forks_chainlink chainlink job_specs create "$ETH_LOG_JOB"
-    const JOB_ID = '7c53e7173ccc4cbd84ce08704e122c99';
-    const jobid = Buffer.from(JOB_ID, 'utf8').toString('hex');
-    console.log('Job id:', jobid);
+    const rawJobPayload = fs.readFileSync('./cypress/fixtures/job.json');
+    const jobPayload = JSON.parse(rawJobPayload);
 
-    const [errClient, responseClient] = await to(axios.get(`${HOST}/deploy/client?oracle=${oracle}&token=${token}&jobid=0x${jobid}`));
+    // patch oracle address
+    jobPayload.initiators[0].params.address = oracle;
+
+    const newJob = await axios.post(`${CHAINLINK_HOST}/v2/specs`,
+        jobPayload,
+        {
+            headers: {
+                cookie: cookies.join(';'),
+            }
+        });
+
+    const rawJobId = newJob?.data?.data?.id;
+
+    if (!rawJobId) {
+        throw new Error('Something wrong with new job');
+    }
+
+    const jobId = '0x' + Buffer.from(rawJobId, 'utf8').toString('hex');
+    console.log('Job id:', rawJobId, ' -> ', jobId);
+
+    const [errClient, responseClient] = await to(axios.get(`${HOST}/deploy/client?oracle=${oracle}&token=${token}&jobid=${jobId}`));
     if (errClient) {
-        throw new Error(errClient.message);
+        throw new Error(errClient?.message);
     }
 
     const client = (responseClient as AxiosResponse).data;
     console.log('Client:', client);
+
+    // TODO: run job
 })();
-
-
-// http://localhost:8080/deploy/client
-// ?oracle=0x98CF278a29EB4788E17725E9dd14150408817c42
-// &token=0x3f7AC08DC07E0aa6cE45420dd980a4862286D07b
-// &jobid=0x3763353365373137336363633463626438346365303837303465313232633939
